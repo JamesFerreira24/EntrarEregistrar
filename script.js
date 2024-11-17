8b9c9cd23ae1718260312056bdf08f3fa7598b8b
JamesFerreira24 <jamesferreiradossantos194@gmail.com>
2024-11-17 16:12:26 -0300

    loginEregister

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const wrapper = document.querySelector('.wrapper');
-const loginlink = document.querySelector('.login-link');
-const registerlink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-Close');
 
-registerlink.addEventListener('click', ()=> {
+registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
-}) 
+});
 
-loginlink.addEventListener('click', ()=> {
+loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
-})
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
